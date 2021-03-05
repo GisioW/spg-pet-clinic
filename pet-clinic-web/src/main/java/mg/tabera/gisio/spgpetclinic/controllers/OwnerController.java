3d169c6781b11a1e9e9bbdb12f839bccffd90115
GisioW <gisio.tabera@gmail.com>
2021-03-05 08:02:38 +0100

    handle id and show list owners and vetes

--- pet-clinic-web/src/main/java/mg/tabera/gisio/spgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/mg/tabera/gisio/spgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package mg.tabera.gisio.spgpetclinic.controllers;
 
+import mg.tabera.gisio.spgpetclinic.sercice.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping(value = {"/","/index", "/index.html"},method = RequestMethod.GET)
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/mg/tabera/gisio/spgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/mg/tabera/gisio/spgpetclinic/controllers/VetController.java
@@ -1,5 +1,6 @@
 package mg.tabera.gisio.spgpetclinic.controllers;
 
+import mg.tabera.gisio.spgpetclinic.sercice.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping(value = {"/vets","/vets/index", "/vets/index.html"},method = RequestMethod.GET)
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
